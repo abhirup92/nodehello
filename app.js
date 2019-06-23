87be1fd6a60d07b17142de3b58e8c24abeb5833d
saikatdas <dassaikat93@yahoo.com>
2019-06-23 23:41:17 +0530

    updated console output

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World- Dev\n');
+  res.end('Hello World- Vivek Devan\n');
 });
 
 server.listen(port, hostname, () => {
