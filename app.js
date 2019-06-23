8de9d67016893167d2ff7852c9607e91fc1dbd3e
saikatdas <dassaikat93@yahoo.com>
2019-06-24 01:34:58 +0530

    removed json data testing

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ const http = require('http');
 
 const hostname = '0.0.0.0';
 const port = 8000;
-const data = require('./data-items.json')
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  //res.end('Hello World- Vivek Devan\n');
-  res.end(data-items.json);
+  res.end('Hello World- Vivek Devan\n');
+  
 });
 
-app.get('/search', function (req, res) {
-  res.json(data);
-})
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
