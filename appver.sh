6ee9fa7bac5db3503ed7dbf19a4d1f83d11c508c
saikatdas <dassaikat93@yahoo.com>
2019-06-24 02:42:39 +0530

    hope

--- appver.sh
+++ appver.sh
@@ -1,4 +1,6 @@
 #create unique application version
+foldername="deployment-"${BUILD_NUMBER}
+appversion="v-"${BUILD_NUMBER}
 aws elasticbeanstalk create-application-version --application-name node-hello \
     --version-label $appversion \
     --source-bundle S3Bucket="node-hello-docker-eb",S3Key=$foldername.zip \
--- deploy2Dev.sh
+++ deploy2Dev.sh
@@ -1,4 +1,6 @@
 #Deploy to Dev env
+foldername="deployment-"${BUILD_NUMBER}
+appversion="v-"${BUILD_NUMBER}
 aws elasticbeanstalk update-environment --application-name node-hello \
     --environment-name devenv \
     --version-label $appversion --region us-east-1
